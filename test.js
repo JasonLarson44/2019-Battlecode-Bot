441a7973b939a2f1b75794b5bbe211f5d7caf1ee
Michael Humphrey <mhumphree@gmail.com>
2019-02-12 13:19:56 -0800

    Streamlined testing pipeline

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const rewire = require('rewire');
 
-const bot = rewire('./compiled_bot.js');
+const bot = rewire('./test_compiled_bot.js');
 
 describe('Array', function() {
   describe('#indexOf()', function() {
@@ -14,8 +14,9 @@ describe('Array', function() {
 let utilities = bot.__get__("utilities");
 describe("utilities", function() {
   describe("#getDistance()", function() {
-    it("should return 5 for points (2, 2) and (5, 6)", function() {
+    it("should return the square of the distance between two points", function() {
       assert.equal(utilities.getDistance({x:2,y:2}, {x:5,y:6}), 25);
-    })
-  })
+      assert.equal(utilities.getDistance({x:-2,y:-3}, {x:-1,y:-5}), 5);
+    });
+  });
 });
