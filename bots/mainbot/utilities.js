6c3254abcb8b8f05e3d9cba8d486e2342a1cbc87
Michael Humphrey <mhumphree@gmail.com>
2019-03-06 10:57:40 -0800

    Fixed phantom churches

--- bots/mainbot/utilities.js
+++ bots/mainbot/utilities.js
@@ -3,6 +3,14 @@ import {SPECS} from 'battlecode';
 
 const utilities = {};
 
+const LABELS = {};
+LABELS[SPECS.CASTLE]   = "CASTLE";
+LABELS[SPECS.CHURCH]   = "CHURCH";
+LABELS[SPECS.PILGRIM]  = "PILGRIM";
+LABELS[SPECS.CRUSADER] = "CRUSADER";
+LABELS[SPECS.PROPHET]  = "PROPHET";
+LABELS[SPECS.PREACHER] = "PREACHER";
+
 // Returns true if the location is a) passable and b) the current robot can not
 // see any robot in that location.
 // NOTE: If the location is outside of the current robot's vision range and a
@@ -17,7 +25,7 @@ utilities.isOpen = (self, location) => {
 
 // Prepend the round number before logging. May extend to log more information
 utilities.log = (self, message) => {
-	self.log(`Round ${self.me.turn} - ${message}`);
+	self.log(`Round ${self.me.turn} - ${LABELS[self.me.unit]} - ${message}`);
 };
 
 utilities.enemiesInRange = (self) => {
@@ -52,6 +60,11 @@ utilities.getDistance = (start, end) => {
 // Returns true if loc2 is the same or one of the eight adjacent cells to loc1
 utilities.isAdjacent = (loc1, loc2) => {
 	return Math.abs(loc1.x - loc2.x) <= 1 && Math.abs(loc1.y - loc2.y) <= 1;
+}
+
+// Returns true if loc2 is on one of the four sides of the loc1
+utilities.isBeside = (loc1, loc2) => {
+	return Math.abs(loc1.x - loc2.x) + Math.abs(loc1.y - loc2.y) <= 1;
 }
 
 // Returns true if loc can be moved to this turn
